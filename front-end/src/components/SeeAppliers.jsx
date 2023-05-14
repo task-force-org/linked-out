448f0f76c407e91c81934dafdde228dcfddf9388
alaa-farhati <farhatiala7@gmail.com>
2023-05-14 20:53:12 +0100

    done

--- front-end/src/components/SeeAppliers.jsx
+++ front-end/src/components/SeeAppliers.jsx
@@ -1,91 +1,76 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 import { useLocation } from "react-router-dom";
-import {useNavigate} from 'react-router-dom'
-import EditProfile from './EditProfil'
+import { useNavigate } from "react-router-dom";
+import EditProfile from "./EditProfil";
 import "bootstrap/dist/css/bootstrap.css";
-import "../css/CDetails.css"
+import "../css/CDetails.css";
 import {
-    MDBCard,
-    MDBCardImage,
-    MDBCardBody,
-    MDBCardTitle,
-    MDBCardText,
-    MDBRow,
-    MDBCol,
-    MDBCardFooter
-  } from 'mdb-react-ui-kit';
-  import Footer from "./Footer";
+  MDBCard,
+  MDBCardImage,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBRow,
+  MDBCol,
+  MDBCardFooter,
+} from "mdb-react-ui-kit";
+import Footer from "./Footer";
 import Navbar from "./Navbar";
 
-
-function SeeAppliers(){
+function SeeAppliers() {
   const location = useLocation();
   const id = location.state.idP["idposts-company"];
-console.log(id,"idp")
-    const [data,setData]=useState([])
+  console.log(id, "idp");
+  const [data, setData] = useState([]);
 
-   const getAppliers=()=>{
-    axios.get(`http://localhost:5000/app/applications/${id}`)
-    .then((res)=>{
-      console.log(res.data)
-setData(res.data)
-    })
-    .catch((err)=>console.log(id,'ID'))
-   }
-   useEffect(()=>{
-    getAppliers()
-   },[])
-   if(!data[0]){
-    console.log(data,'data')
-    return <h1>no appliers for the time being</h1>
-      
-     
-      
-     
-   
-    
-   }else if(data!==[]) return (
-   
-   <>
-   {data.map((e)=>{
-    return (<>
-     
-      <MDBRow className='row-cols-1 row-cols-md-2 g-4'>
-      <MDBCol>
-        <MDBCard>
-          <MDBCardImage
-            src={e.profile_pic}
-            alt='...'
-            position='top'
-          />
-          <MDBCardBody>
-            <MDBCardTitle>{e.full_name }</MDBCardTitle>
-            <MDBCardText>
-            bio:{e.bio}
-            </MDBCardText>
-            <MDBCardText>
-            Experiences:{e.experiences }
-            </MDBCardText>
-            <MDBCardText>
-            Education:{e.education }
-            </MDBCardText>
-          </MDBCardBody>
-          <MDBCardFooter>
-            <small className='text-muted'> Email:{e.email }</small>
-          </MDBCardFooter>
-        </MDBCard>
-      </MDBCol>
-     
-    </MDBRow>
+  const getAppliers = () => {
+    axios
+      .get(`http://localhost:5000/app/applications/${id}`)
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch((err) => console.log(id, "ID"));
+  };
+  useEffect(() => {
+    getAppliers();
+  }, []);
+  if (!data[0]) {
+    console.log(data, "data");
+    return <h1>no appliers for the time being</h1>;
+  } else if (data !== [])
+    return (
+      <>
+        {data.map((e) => {
+          return (
+            <>
+              <MDBRow className="row-cols-1 row-cols-md-2 g-4">
+                <MDBCol>
+                  <MDBCard>
+                    <MDBCardImage
+                      src={e.profile_pic}
+                      alt="..."
+                      position="top"
+                    />
+                    <MDBCardBody>
+                      <MDBCardTitle>{e.full_name}</MDBCardTitle>
+                      <MDBCardText>bio:{e.bio}</MDBCardText>
+                      <MDBCardText>Experiences:{e.experiences}</MDBCardText>
+                      <MDBCardText>Education:{e.education}</MDBCardText>
+                    </MDBCardBody>
+                    <MDBCardFooter>
+                      <small className="text-muted"> Email:{e.email}</small>
+                    </MDBCardFooter>
+                  </MDBCard>
+                </MDBCol>
+              </MDBRow>
+            </>
+          );
+        })}
       </>
-    )
-   })}
-  
-   </>)
-  
-    
+    );
 }
 
-export default SeeAppliers
+export default SeeAppliers;
--- front-end/src/components/companyDetails.jsx
+++ front-end/src/components/companyDetails.jsx
@@ -1,54 +1,30 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import {useNavigate} from 'react-router-dom'
-import "bootstrap/dist/css/bootstrap.css";
-import "../css/CDetails.css"
-import Button from 'react-bootstrap/Button';
-
-import {
-    MDBCard,
-    MDBCardImage,
-    MDBCardBody,
-    MDBCardTitle,
-    MDBCardText,
-    MDBRow,
-    MDBCol
-  } from 'mdb-react-ui-kit';
-  import Footer from "./Footer";
-import Navbar from "./Navbar";
-import EditProfileCompany from "./EditProfileCompany"
-
-
-
 function CompanyDetails() {
-  const navigate=useNavigate()
+  const navigate = useNavigate();
   const [data, setData] = useState({});
   const [posts, setPosts] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const location = useLocation();
-  var id = location.state.data.idcompany ;//this condition need to be checked 
+  var id = location.state.data.idcompany; //this condition need to be checked
 
-console.log(id,"data")
+  console.log(id, "data");
 
   const handleDeletePost = (event, postID) => {
-    console.log(postID)
+    console.log(postID);
     const url = `http://localhost:5000/company/post/${postID}`;
     axios
       .delete(url, {
         headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`, 
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
       })
       .then(() => {
-        console.log('Post deleted successfully');
+        console.log("Post deleted successfully");
       })
       .catch((err) => {
-        console.error('Error deleting post:', err);
+        console.error("Error deleting post:", err);
       });
-      window.location.reload()
+    window.location.reload();
   };
-  
 
   const handleEditProfile = () => {
     setIsEditing(true);
@@ -65,15 +41,12 @@ console.log(id,"data")
       .catch((err) => console.error(err));
   };
 
-
-
-
-
-
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const { data } = await axios.get(`http://localhost:5000/company/get/${id}`);
+        const { data } = await axios.get(
+          `http://localhost:5000/company/get/${id}`
+        );
         setData(data);
       } catch (error) {
         console.log(error);
@@ -88,7 +61,7 @@ console.log(id,"data")
       .get(`http://localhost:5000/company/post/${id}`)
       .then(({ data }) => setPosts(data))
       .catch((err) => console.log(id));
-      console.log(id)
+    console.log(id);
   }, [id]);
 
   if (Object.keys(data).length === 0) {
@@ -97,76 +70,100 @@ console.log(id,"data")
 
   return (
     <div>
-      <Navbar/>
-    <div>
-  <div className="div">
-    <span className="container">
-      <img className="img" src={data.img} alt="Company Logo" />
-      <h4 className="name">{data.company_name}</h4>
-    </span>
-  </div>
-</div>
-      
-     
+      <Navbar />
+      <div>
+        <div className="div">
+          <span className="container">
+            <img className="img" src={data.img} alt="Company Logo" />
+            <h4 className="name">{data.company_name}</h4>
+          </span>
+        </div>
+      </div>
+
       <h1>{data.description}</h1>
       <h1>{data.email}</h1>
-      {!isEditing && <Button variant="primary" onClick={handleEditProfile}>Edit Profile</Button>}
-        {isEditing && <EditProfileCompany data={[data]} onSave={handleSaveProfile} />}
-{<Button variant="primary" onClick={()=>navigate("/addpostCompony",{ state: { ids: id } })}>add post</Button>}
+      {!isEditing && (
+        <Button variant="primary" onClick={handleEditProfile}>
+          Edit Profile
+        </Button>
+      )}
+      {isEditing && (
+        <EditProfileCompany data={[data]} onSave={handleSaveProfile} />
+      )}
+      {
+        <Button
+          variant="primary"
+          onClick={() => navigate("/addpostCompony", { state: { ids: id } })}
+        >
+          add post
+        </Button>
+      }
 
       {posts.map((post) => {
-
-
-const postDtails={
-  post_title: post.post_title,
-  post_aplliers: post.post_aplliers,
-  posts_details: post.posts_details,
-  post_img: post.post_img,
-  post_description:post.post_description,
-  post_date: post.post_date,
-  company_name: data.company_name,
-  img: data.img,
-  idcompany: data.idcompany,
-  "idposts-company":post["idposts-company"]
-}
-
+        const postDtails = {
+          post_title: post.post_title,
+          post_aplliers: post.post_aplliers,
+          posts_details: post.posts_details,
+          post_img: post.post_img,
+          post_description: post.post_description,
+          post_date: post.post_date,
+          company_name: data.company_name,
+          img: data.img,
+          idcompany: data.idcompany,
+          "idposts-company": post["idposts-company"],
+        };
 
         return (
           <>
-           { console.log(post,"post")}
-          <MDBRow className="row-cols-1 row-cols-md-2 g-4">
-          <MDBCol>
-            {}
-            <MDBCard onClick={()=>{
-             
-              navigate("/PostDtails", { state: { data: postDtails } })} }>
-              <MDBCardImage src={post.post_img} alt="..." position="top" />
-              <MDBCardBody>
-                <MDBCardTitle>{post.post_title}</MDBCardTitle>
-                <MDBCardText>{post.post_description}</MDBCardText>
-              </MDBCardBody> 
-              </MDBCard><Button variant="primary" onClick={(event)=>navigate("/SeeAppliers", { state: { idP: post } })}>see appliers</Button>
-          </MDBCol> <Button variant="primary" onClick={(event)=>handleDeletePost(event,post["idposts-company"])}>delete post</Button>
-        </MDBRow> <Button variant="primary" onClick={()=>navigate("/SeeAppliers",{ state: { id:post["idposts-company"] } })}>add post</Button>
-        </>
-        )
-      }
-        
-       
-      )}
-      <Footer/>
+            {console.log(post, "post")}
+            <MDBRow className="row-cols-1 row-cols-md-2 g-4">
+              <MDBCol>
+                {}
+                <MDBCard
+                  onClick={() => {
+                    navigate("/PostDtails", { state: { data: postDtails } });
+                  }}
+                >
+                  <MDBCardImage src={post.post_img} alt="..." position="top" />
+                  <MDBCardBody>
+                    <MDBCardTitle>{post.post_title}</MDBCardTitle>
+                    <MDBCardText>{post.post_description}</MDBCardText>
+                  </MDBCardBody>
+                </MDBCard>
+                <Button
+                  variant="primary"
+                  onClick={(event) =>
+                    navigate("/SeeAppliers", { state: { idP: post } })
+                  }
+                >
+                  see appliers
+                </Button>
+              </MDBCol>{" "}
+              <Button
+                variant="primary"
+                onClick={(event) =>
+                  handleDeletePost(event, post["idposts-company"])
+                }
+              >
+                delete post
+              </Button>
+            </MDBRow>{" "}
+            <Button
+              variant="primary"
+              onClick={() =>
+                navigate("/SeeAppliers", {
+                  state: { id: post["idposts-company"] },
+                })
+              }
+            >
+              add post
+            </Button>
+          </>
+        );
+      })}
+      <Footer />
     </div>
   );
 }
 
 export default CompanyDetails;
-
-
-
-
-
-
-
-
-
-
